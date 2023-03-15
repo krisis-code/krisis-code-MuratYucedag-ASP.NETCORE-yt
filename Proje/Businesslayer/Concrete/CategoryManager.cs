239466323d71fe2e7c7f5cb8c83dc11c41ba7a1d
krisis-code <menesbaspinar@hotmail.com>
2023-03-15 17:48:20 +0300

    20 ders tamam revizeler yapıldı

--- Proje/Businesslayer/Concrete/CategoryManager.cs
+++ Proje/Businesslayer/Concrete/CategoryManager.cs
@@ -7,36 +7,42 @@ using System.ComponentModel.DataAnnotations;
 using Businesslayer.Abstract;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 
 namespace Businesslayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
+        EfCategoryRepository efCategoryRepository;
+
+        public CategoryManager()
+        {
+            efCategoryRepository = new EfCategoryRepository();
+        }
         public void CategoryAdd(Category category)
         {
-            throw new NotImplementedException(); 
+            efCategoryRepository.Insert(category);
 
         }
 
         public void CategoryDelete(Category category)
         {
-            throw new NotImplementedException();
+            efCategoryRepository.Delete(category);
         }
 
         public Category CategoryGetById(int id)
         {
-            throw new NotImplementedException();
+            return efCategoryRepository.GetById(id);
         }
 
         public List<Category> CategoryList()
         {
-            throw new NotImplementedException();
+            return efCategoryRepository.GetListAll();
         }
 
         public void CategoryUpdate(Category category)
         {
-            throw new NotImplementedException();
+            efCategoryRepository.Update(category);
         }
     }
 }
---
+++ Proje/Muratyucedag/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace muratyucedag.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
